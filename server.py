d37184a264d3e51a70817204695d592249f6e0e0
Sarath M <sarathm00@gmail.com>
2015-08-03 14:01:10 +0530

    Added db support

--- server.py
+++ server.py
@@ -1,37 +1,65 @@
 import socket
 import serial
+import sqlite3
 
-def recvdata():
-	s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-	s.bind(('0.0.0.0', 8000))
-	s.listen(1)
-	print "Waiting for Client.."
-	var1,var2=s.accept()
-	print "Connected to Client & waiting for data."
-	packet=var1.recv(100)
-	print "Received Data: ", packet
-	s.close()
-	return packet
+class database():
 
-def senddata():
-	s=serial.Serial('/dev/ttyUSB0', 9600, timeout=1)
-	data=extractdata()
-	print data
-	
+	def __init__(self):
+		self.s=sqlite3.connect('report.db')
+		self.c=self.s.cursor()
 
-def extractdata():
-	data=recvdata()
-	data=data.split(';')
-	sms='EmpID: ' +data[0]+'\nDpt ID: ' +data[1]+'\nCode: '+data[2]
-	return sms
+	def insertData(self,eid,dpt,code):
+		self.c.execute('''INSERT INTO tb(empid,dept,code) VALUES(?,?,?)''',(eid,dpt,code))
+		self.s.commit()
 
+	def close():
+		self.s.close()	
+
+class employee():
+	def __init__(self):
+
+		self.db=database()
+
+		#self.xbee = serial.Serial('/dev/ttyUSB0', 9600, timeout = 1)
+
+		self.s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+		self.s.bind(('0.0.0.0', 8000))
+		self.s.listen(1)
+		self.sock,self.var2 = self.s.accept()
+		
+	def main(self):
+		packet = self.recvdata()
+		sms = self.extractdata(packet)
+		self.senddata(sms)
+
+	def recvdata(self):
+		while True:
+			packet = self.sock.recv(100)
+			
+			if len(packet)>0:
+				print "Received Data: ", packet
+				return packet
+
+	def senddata(self,sms):
+		print sms
+		
+	def extractdata(self,packet):
+		data = packet.split(';')
+
+		self.db.insertData(data[0],data[1],data[2])
+
+		sms = 'EmpID: ' +data[0]+'\nDpt ID: ' +data[1]+'\nCode: '+data[2]
+		return sms
+
+emp=employee()
 while True:
-	senddata()
+	emp.main()
 
+		
 
+		
 
-
-
-
-
-
+	
+		
+		
+		
