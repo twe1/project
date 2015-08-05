931dc1e575087f5b33dc11ebd746246761ad0551
Sarath M <sarathm00@gmail.com>
2015-08-05 12:42:57 +0530

    debugging

--- clientdpt.py
+++ clientdpt.py
@@ -1,8 +1,11 @@
 import serial
+import time
 s=serial.Serial('/dev/ttyUSB1', 9600, timeout=1)
 
 while True:
 	fData=s.read(100)
+	time.sleep(1)
+	print fData, '.', 
 	if len(fData)>0:
 		s.write(fData)
 		print 'sending: ',fData
